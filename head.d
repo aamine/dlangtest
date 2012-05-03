662607243a2636545de9a528b21957ced627089e
Minero Aoki <mineroaoki@gmail.com>
2012-05-04 04:34:58 +0900

    do not read too much lines.

--- head.d
+++ head.d
@@ -5,9 +5,10 @@ void
 main(string[] args)
 {
     auto n = parse!uint(args[1]);
+    if (n == 0) return;
     foreach (string line; stdin.lines) {
-        if (n <= 0) break;
         write(line);
         n--;
+        if (n <= 0) break;
     }
 }
